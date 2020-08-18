3ecf38fdc9fe2fa62254f53b24b7dd19a2161418
dangulos <dangulos@unal.edu.co>
2020-08-18 14:58:09 -0500

    sh sh

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -16,15 +16,22 @@ apt-get install -y kubeadm kubectl kubelet kubernetes-cni
 #disable swapoff
 swapoff -a
 #kube init
-kubeadm init
+sudo kubeadm init
 #remove taint in master & creds
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/master-
+sudo kubectl taint nodes --all node-role.kubernetes.io/master-
 #cilium | network overlay
-kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.8/install/kubernetes/quick-install.yaml
+sudo kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.8/install/kubernetes/quick-install.yaml
 #BareMetal metallb
 # vease: https://metallb.universe.tf/installation/
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 # nginx-ingress
 # después de metallb kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/baremetal/deploy.yaml
